b745bd89504257ad0c7eda145d60cca8a20929a2
Brenda Goodwin <brendanced@gmail.com>
2020-11-20 15:26:41 -0700

    Writes to team file

--- Develop/app.js
+++ Develop/app.js
@@ -43,6 +43,7 @@ function sortRole() {
       }
     });
 }
+
 sortRole();
 
 //manager flow
@@ -110,118 +111,109 @@ function anotherOne() {
           createTeam();
       }
     });
-
-  //adds an engineer
-  function createEngineer() {
-    inquirer
-      .prompt([
-        {
-          type: "input",
-          name: "name",
-          message: "Engineer's name.",
-          default: "firstName lastName",
-        },
-        {
-          type: "input",
-          name: "id",
-          message: "Engineer's employer id no.",
-          default: "employee number",
-        },
-        {
-          type: "input",
-          name: "email",
-          message: "Engineer's email address",
-          default: "email@address.com",
-        },
-        {
-          type: "input",
-          name: "github",
-          message: "Engineer's gitHub profile link",
-          default: "https://github.com/username",
-        },
-      ])
-      .then((response) => {
-        const engineer = new Engineer(
-          response.name,
-          response.id,
-          response.email,
-          response.github
-        );
-        teamMember.push(engineer);
-        console.log(teamMember);
-        anotherOne();
-      });
-  }
-
-  //adds an intern
-  function createIntern() {
-    inquirer
-      .prompt([
-        {
-          type: "input",
-          name: "name",
-          message: "Intern's name.",
-          default: "firstName lastName",
-        },
-        {
-          type: "input",
-          name: "id",
-          message: "Intern's employer id no.",
-          default: "employee number",
-        },
-        {
-          type: "input",
-          name: "email",
-          message: "Intern's email address",
-          default: "email@address.com",
-        },
-        {
-          type: "input",
-          name: "school",
-          message: "Intern's school?",
-          default: "school name",
-        },
-      ])
-      .then((response) => {
-        const intern = new Intern(
-          response.name,
-          response.id,
-          response.email,
-          response.school
-        );
-        teamMember.push(intern);
-        console.log(teamMember);
-        anotherOne();
-      });
-  }
-
-  render(teamMember);
-
-  function createTeam() {
-    if (!fs.existsSync(OUTPUT_DIR)) {
-      fs.mkdirSync(OUTPUT_DIR);
-    } else {
-      fs.writeFileSync(outputPath, render(teamMember), "utf8");
-    }
-  }
-
-  // After the user has input all employees desired, call the `render` function (required
-  // above) and pass in an array containing all employee objects; the `render` function will
-  // generate and return a block of HTML including templated divs for each employee!
-
-  // After you have your html, you're now ready to create an HTML file using the HTML
-  // returned from the `render` function. Now write it to a file named `team.html` in the
-  // `output` folder. You can use the variable `outputPath` above target this location.
-  // Hint: you may need to check if the `output` folder exists and create it if it
-  // does not.
-
-  // HINT: each employee type (manager, engineer, or intern) has slightly different
-  // information; write your code to ask different questions via inquirer depending on
-  // employee type.
-
-  // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-  // and Intern classes should all extend from a class named Employee; see the directions
-  // for further information. Be sure to test out each class and verify it generates an
-  // object with the correct structure and methods. This structure will be crucial in order
-  // for the provided `render` function to work!
 }
+
+//adds an engineer
+function createEngineer() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Engineer's name.",
+        default: "firstName lastName",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Engineer's employer id no.",
+        default: "employee number",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Engineer's email address",
+        default: "email@address.com",
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "Engineer's gitHub profile link",
+        default: "https://github.com/username",
+      },
+    ])
+    .then((response) => {
+      const engineer = new Engineer(
+        response.name,
+        response.id,
+        response.email,
+        response.github
+      );
+      teamMember.push(engineer);
+      console.log(teamMember);
+      anotherOne();
+    });
+}
+
+//adds an intern
+function createIntern() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Intern's name.",
+        default: "firstName lastName",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Intern's employer id no.",
+        default: "employee number",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Intern's email address",
+        default: "email@address.com",
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "Intern's school?",
+        default: "school name",
+      },
+    ])
+    .then((response) => {
+      const intern = new Intern(
+        response.name,
+        response.id,
+        response.email,
+        response.school
+      );
+      teamMember.push(intern);
+      console.log(teamMember);
+      anotherOne();
+    });
+}
+
+// creates the team in dir_output
+function createTeam() {
+  if (!fs.existsSync(OUTPUT_DIR)) {
+    fs.mkdirSync(OUTPUT_DIR);
+  } else {
+    fs.writeFileSync(outputPath, render(teamMember), "utf8");
+  }
+}
+//renders the html
+render(teamMember);
+
+// HINT: each employee type (manager, engineer, or intern) has slightly different
+// information; write your code to ask different questions via inquirer depending on
+// employee type.
+
+// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
+// and Intern classes should all extend from a class named Employee; see the directions
+// for further information. Be sure to test out each class and verify it generates an
+// object with the correct structure and methods. This structure will be crucial in order
+// for the provided `render` function to work!
