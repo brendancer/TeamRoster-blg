01bc234fbda3c036a5f5193135d22e53c91e9cd1
Brenda Goodwin <brendanced@gmail.com>
2020-11-21 14:10:46 -0700

    added and implemented Open pkg

--- Develop/app.js
+++ Develop/app.js
@@ -4,6 +4,7 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const open = require("open");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
@@ -173,6 +174,7 @@ function createTeam() {
   } else {
     fs.writeFileSync(outputPath, render(teamMember), "utf8");
   }
+  open("./output/team.html");
 }
 
 render(teamMember);
